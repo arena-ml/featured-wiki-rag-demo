eae1bcbde9fae38a0ea1645bdb534a368c08784c
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2025-04-16 17:55:24 +0530

    entrypoint silently run ollama

--- devOps/entrypoint.sh
+++ devOps/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Start Ollama in background if not running
-pgrep -f "ollama serve" > /dev/null || nohup ollama serve > /var/log/ollama.log 2>&1 &
-
-# Launch shell
-exec /bin/bash
+# Start Ollama in background 
+ (ollama serve > /dev/null 2>&1 &)
