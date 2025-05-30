4796fccba3c2725db4f94942c08795c6ba19128d
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2025-05-30 13:46:07 +0530

    misc fix

--- llm2-gen-summaries-zeroshot/llm2-gen-summaries-zeroshot.py
+++ llm2-gen-summaries-zeroshot/llm2-gen-summaries-zeroshot.py
@@ -18,7 +18,7 @@ CONST_MAX_CTX=8200
 
 # Paths
 
-articles_file_path = "WWikiRC_StepOne.json"
+articles_file_path = "WikiRC_StepOne.json"
 output_file_path = "llm2-summaries-using-zeroshot.json "
 
 
