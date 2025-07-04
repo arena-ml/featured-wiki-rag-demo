2eb11c17fbd891eab52942fb8bc93abfc8b303ab
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2025-07-04 14:55:28 +0530

    test new script

--- fetch-wiki-data/fetchWikiData.py
+++ fetch-wiki-data/fetchWikiData.py
@@ -28,9 +28,13 @@ logger = logging.getLogger("fetch_wikiArticles")
 # Configure session for reuse
 session = requests.Session()
 session.headers.update(
+    # {
+    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    # }
     {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        "User-Agent": "MyWikiApp/1.0 (myemail@example.com)"
     }
+
 )
 
 
